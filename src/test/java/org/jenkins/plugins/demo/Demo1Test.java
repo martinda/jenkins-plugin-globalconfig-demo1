7f911ec1f94e28d0aed2c194cfb9533fc5ef4048
Martin d'Anjou <martin.danjou14@gmail.com>
2016-02-24 11:29:27 -0500

    Test global variables

--- src/test/java/org/jenkins/plugins/demo/Demo1Test.java
+++ src/test/java/org/jenkins/plugins/demo/Demo1Test.java
@@ -3,6 +3,9 @@ package org.jenkins.plugins.demo;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -29,6 +32,33 @@ public class Demo1Test {
 
         // Check expectations
         j.assertBuildStatusSuccess(build);
+
+    }
+
+    @Test
+    public void globalConfig() throws Exception {
+
+        Demo1 demo1 = new Demo1("text");
+
+        DemoGlobalConfig cfg = DemoGlobalConfig.get();
+        cfg.setGlobalVar("this is a global string");
+
+        List<MyString> myStrings = new ArrayList<>();
+        myStrings.add(new MyString("string 1"));
+        myStrings.add(new MyString("string 2"));
+        cfg.setMyStrings(myStrings);
+
+        // Run build
+        FreeStyleProject project = j.createFreeStyleProject();
+        project.getBuildersList().add(demo1);
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+
+        // Check expectations
+        j.assertBuildStatusSuccess(build);
+        j.assertLogContains("Input 1: text", build);
+        j.assertLogContains("Global Var: this is a global string", build);
+        j.assertLogContains("String value: string 1", build);
+        j.assertLogContains("String value: string 2", build);
 
     }
 
